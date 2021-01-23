78316b49194e4193a27b36332ff732b5d2245837
Andre Smith <andre.frank.smith@gmail.com>
2021-01-23 15:05:01 +0200

    fixing deploy issue

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/grand-west-casino-online'));
+app.use(express.static(__dirname + './dist/grand-west-casino-online'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/grand-west-casino-online/index.html'));
+res.sendFile(path.join(__dirname+'./dist/grand-west-casino-online/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
